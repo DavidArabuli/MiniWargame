052e6e8f63765d0156a40371753d3fab23ffbb0c
David Arabuli <davidsarabuli@gmail.com>
2026-09-12 15:03:10 +0300

    Grid and mouse click tracking

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ namespace MiniWargame
 {
     public partial class MainWindow : Window
     {
+        private List<GameSpot> gameSpots = new List<GameSpot>();
         // Board settings
         private const int TileSize = 50;
         private const int BoardWidth = 10;
@@ -29,31 +30,58 @@ namespace MiniWargame
         // Creates the tiles
         private void CreateBoard()
         {
-            for (int x = 0; x < 2; x++)
+            for (int x = 0; x < 5; x++)
             {
-                for (int y = 0; y < 2; y++)
+                for (int y = 0; y < 5; y++)
                 {
+                    int spotX = x * Spacing;
+                    int spotY = y * Spacing;
+
+                    GameSpot gameSpot = new GameSpot();
+                    gameSpot.X = spotX;
+                    gameSpot.Y = spotY;
+                    gameSpots.Add(gameSpot);
+
                     Ellipse spot = new Ellipse();
-                    spot.Width = 10;
-                    spot.Height = 10;
-
-                    Canvas.SetLeft(spot, Spacing * x);
-                    Canvas.SetTop(spot, Spacing * y);
-
+                    spot.Width = 20;
+                    spot.Height = 20;
                     spot.Fill = Brushes.Black;
+
+                    Canvas.SetLeft(spot, spotX);
+                    Canvas.SetTop(spot, spotY);
+
                     GameBoard.Children.Add(spot);
                 }
-                
-
             }
-            
+        }
+        class GameSpot
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
         }
         private void GameBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Point mousePosition = e.GetPosition(GameBoard);
-            MessageBox.Show($"X: {mousePosition.X}, Y: {mousePosition.Y}");
-        }
 
+            foreach (GameSpot gameSpot in gameSpots)
+            {
+                double distanceX = mousePosition.X - gameSpot.X;
+                double distanceY = mousePosition.Y - gameSpot.Y;
+
+                double distance = Math.Sqrt(
+                    distanceX * distanceX +
+                    distanceY * distanceY
+                );
+
+                if (distance < 50)
+                {
+                    Canvas.SetLeft(Player, gameSpot.X - Player.Width / 2 + 10);
+                    Canvas.SetTop(Player, gameSpot.Y - Player.Height / 2 + 10);
+
+                    break;
+                }
+            }
+        }
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             Player.Fill = Brushes.Green;
